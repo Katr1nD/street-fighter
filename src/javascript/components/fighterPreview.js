8bb1d4e07facbb99d0a9c95d09cbe1b6a944a85b
Katr1nD <kathrin.drako@gmail.com>
2020-05-08 23:21:43 +0300

    Add fighters preview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -6,18 +6,37 @@ export function createFighterPreview(fighter, position) {
     tagName: 'div',
     className: `fighter-preview___root ${positionClassName}`,
   });
-
   // todo: show fighter info (image, name, health, etc.)
+  const fighterMarkup = `<img src=${fighter.source}>
+    <table style="margin: 0 auto; color: white; font-size: 20px; font: Arial">
+      <caption>
+        <p>${fighter.name}</p>
+      </caption>
+      <tr>
+        <td>Health:</td>
+      <td>${fighter.health}</td>
+      </tr>
+      <tr>
+        <td>Attack:</td>
+      <td>${fighter.attack}</td>
+      </tr>
+      <tr>
+        <td>Defense:</td>
+      <td>${fighter.defense}</td>
+      </tr>
+    </table>`;
+
+  fighterElement.innerHTML = fighterMarkup;
 
   return fighterElement;
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
+  const attributes = {
+    src: source,
     title: name,
-    alt: name 
+    alt: name
   };
   const imgElement = createElement({
     tagName: 'img',
--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,6 +2,7 @@ import { createElement } from '../helpers/domHelper';
 import { renderArena } from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import { fighterService } from '../services/fightersService';
 
 export function createFightersSelector() {
   let selectedFighters = [];
@@ -21,13 +22,16 @@ const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
   // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+  const fighterInfo = await fighterService.getFighterDetails(fighterId);
+  fighterDetailsMap.set(fighterInfo.name, fighterInfo);
+  return fighterInfo;
 }
 
 function renderSelectedFighters(selectedFighters) {
   const fightersPreview = document.querySelector('.preview-container___root');
   const [playerOne, playerTwo] = selectedFighters;
   const firstPreview = createFighterPreview(playerOne, 'left');
-  const secondPreview = createFighterPreview(playerTwo, 'right');
+  const secondPreview = Boolean(playerTwo) ? createFighterPreview(playerTwo, 'right') : null;
   const versusBlock = createVersusBlock(selectedFighters);
 
   fightersPreview.innerHTML = '';
