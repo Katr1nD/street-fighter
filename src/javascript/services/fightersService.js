4459200a15435361eb90fa4990567e72e70e229e
Katr1nD <kathrin.drako@gmail.com>
2020-05-08 21:36:44 +0300

    created get fighter details method

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -1,4 +1,6 @@
-import { callApi } from '../helpers/apiHelper';
+import {
+  callApi
+} from '../helpers/apiHelper';
 
 class FighterService {
   async getFighters() {
@@ -13,6 +15,13 @@ class FighterService {
   }
 
   async getFighterDetails(id) {
+    try {
+      const endpoint = `details/fighter/${id}.json`;
+      const apiResult = await callApi(endpoint, 'GET');
+      return apiResult;
+    } catch (error) {
+      throw error;
+    }
     // todo: implement this method
     // endpoint - `details/fighter/${id}.json`;
   }
