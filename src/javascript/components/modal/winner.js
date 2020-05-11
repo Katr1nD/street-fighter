7483b6449e59f642bb68301239c193184dfc4cd5
Katr1nD <kathrin.drako@gmail.com>
2020-05-11 22:16:06 +0300

    implement final modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,9 @@
+import { showModal } from "./modal";
 export function showWinnerModal(fighter) {
   // call showModal function 
+
+  const title = `${fighter.name} wins!`;
+  const bodyElement = createElement({ tagName: 'p', className: 'modal-body' });
+  bodyElement.innerText = `${fighter.name} defeated his enemy`;
+  showModal({ title, bodyElement });
 }
